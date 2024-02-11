f6b0ce20375e919526f1d02e08ed2b7d934c5924
Paul Thrasher <thrashr888@gmail.com>
2024-02-11 03:26:13 -0800

    Added generateEmbeddings (#11)
    For generating embeddings using Ollama's API endpoint: https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings

---
+++ Sources/OllamaKit/Responses/OKGenerateEmbeddingsResponse.swift
@@ -0,0 +1,15 @@
+//
+//  OKGenerateEmbeddingsResponse.swift
+//
+//
+//  Created by Paul Thrasher on 02/09/24.
+//
+
+import Foundation
+
+/// A structure that represents the response to an embedding request from the Ollama API.
+public struct OKGenerateEmbeddingsResponse: Decodable {
+    
+    /// An array of floats representing the embeddings of the input prompt.
+    public let embedding: [Float]?
+}
