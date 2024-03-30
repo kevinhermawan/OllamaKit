c366a7e6b8b1b1179b1fcbdc0aae7c264ec772e9
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2024-03-30 15:37:58 +0700

    feat: adds initializer with default base URL

--- Sources/OllamaKit/OllamaKit.swift
+++ Sources/OllamaKit/OllamaKit.swift
@@ -8,20 +8,30 @@
 import Foundation
 
 /// Provides a streamlined way to access the Ollama API, encapsulating the complexities of network communication and data processing.
-///
-/// Usage of ``OllamaKit`` involves initializing it with the base URL of the Ollama API. This setup configures the internal router and decoder for handling API interactions.
-///
-/// ```swift
-/// let baseURL = URL(string: "http://localhost:11434")!
-/// let ollamaKit = OllamaKit(baseURL: baseURL)
-/// ```
-///
-/// - Initialization:
-///   - `init(baseURL: URL)`: Initializes a new instance of ``OllamaKit`` with the provided base URL for the Ollama API.
 public struct OllamaKit {
     var router: OKRouter.Type
     var decoder: JSONDecoder = .default
     
+    /// Initializes a new instance of `OllamaKit` with the default base URL for the Ollama API.
+    ///
+    /// ```swift
+    /// let ollamaKit = OllamaKit()
+    /// ```
+    public init() {
+        let router = OKRouter.self
+        router.baseURL = URL(string: "http://localhost:11434")!
+        
+        self.router = router
+    }
+    
+    /// Initializes a new instance of `OllamaKit` with a custom base URL for the Ollama API.
+    ///
+    /// ```swift
+    /// let customBaseURL = URL(string: "https://api.customollama.com")!
+    /// let ollamaKit = OllamaKit(baseURL: customBaseURL)
+    /// ```
+    ///
+    /// - Parameter baseURL: The base URL to use for API requests.
     public init(baseURL: URL) {
         let router = OKRouter.self
         router.baseURL = baseURL
