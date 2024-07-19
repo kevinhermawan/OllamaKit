f6b5e712189c215e26e148e79b118a2906d194fa
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2024-07-19 14:52:00 +0700

    chore: changes minimum macOS version to 12 (#30)

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "OllamaKit",
     platforms: [
         .iOS(.v15),
-        .macOS(.v13),
+        .macOS(.v12),
         .macCatalyst(.v15)
     ],
     products: [
