47466a6cdf9c1ff0a50c6961020e4127a42a6d5e
Lukas Pistrol <lukas@pistrol.com>
2024-11-25 17:02:32 +0100

    add details to `OKModelResponse`

--- Sources/OllamaKit/Responses/OKModelResponse.swift
+++ Sources/OllamaKit/Responses/OKModelResponse.swift
@@ -25,5 +25,26 @@ public struct OKModelResponse: Decodable {
         
         /// A `Date` representing the last modification date of the model.
         public let modifiedAt: Date
+
+        /// The details about the model.
+        public let details: ModelDetails
+
+        /// A structure that represents the details of the model.
+        public struct ModelDetails: Decodable {
+            /// The format of the model. E.g. "gguf".
+            public let format: String
+
+            /// The family of the model. E.g. "llama".
+            public let family: String
+
+            /// The parameter size of the model. E.g. "8.0B".
+            public let parameterSize: String
+
+            /// The quantization level of the model. E.g. "Q4_0".
+            public let quantizationLevel: String
+
+            /// All the families of the model. E.g. ["llama", "phi3"].
+            public let families: [String]?
+        }
     }
 }
